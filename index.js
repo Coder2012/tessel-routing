dab9c841e406db4dbe007321d7eaaac8ab7d4d4e
Neil Brown <nbrown@amaze.com>
2016-05-23 11:22:13 +0100

    added chalk for colour debugging

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var router = require('tiny-router');
+const router = require('tiny-router');
+const chalk = require('chalk');
 
-router.get('/', function(req, res){
-	res.send('Reply from the webserver');
+//Setup loggin
+const info = chalk.bold.cyan;
+
+router.get('/', (req, res) => {
+	res.send('Reply from the webserver')
+	console.log(info('Request for /'))
 })
 
-router.get('/stelios', function(req, res){
-	res.send('This is Stelios Issaias private area');
+router.get('/stelios', (req, res) => {
+	res.send('This is Stelios Issaias private area')
+	console.log(info('Request for /stelios'))
 })
 
 router.listen(8080);
